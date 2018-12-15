0b7316ab4d4198348b3dc7c3c61858393e470eba
matt <matt.pocock@thevirtualforge.com>
2018-12-15 20:19:10 +0000

    Updated the index template

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,5 +1,7 @@
 import CMS from 'netlify-cms';
 
 import AboutPagePreview from './preview-templates/AboutPagePreview';
+import HomePagePreview from './preview-templates/HomePagePreview';
 
 CMS.registerPreviewTemplate('about', AboutPagePreview);
+CMS.registerPreviewTemplate('home', HomePagePreview);
