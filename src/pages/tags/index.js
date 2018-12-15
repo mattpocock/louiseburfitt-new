74f42772bfcf3d22d36558cee99bd4a0ae3dbabe
matt <matt.pocock@thevirtualforge.com>
2018-12-15 18:46:26 +0000

    Stripped out the blog components from netlify

--- src/pages/tags/index.js
+++ src/pages/tags/index.js
@@ -1,12 +1,10 @@
-import React from 'react'
-import { kebabCase } from 'lodash'
-import Helmet from 'react-helmet'
-import { Link, graphql } from 'gatsby'
-import Layout from '../../components/Layout'
+import React from 'react';
+import { kebabCase } from 'lodash';
+import Helmet from 'react-helmet';
+import { Link, graphql } from 'gatsby';
+import Layout from '../../components/Layout';
 
-const TagsPage = ({
-  data: { allMarkdownRemark: { group }, site: { siteMetadata: { title } } },
-}) => (
+const TagsPage = ({ title }) => (
   <Layout>
     <section className="section">
       <Helmet title={`Tags | ${title}`} />
@@ -15,25 +13,14 @@ const TagsPage = ({
           <div
             className="column is-10 is-offset-1"
             style={{ marginBottom: '6rem' }}
-          >
-            <h1 className="title is-size-2 is-bold-light">Tags</h1>
-            <ul className="taglist">
-              {group.map(tag => (
-                <li key={tag.fieldValue}>
-                  <Link to={`/tags/${kebabCase(tag.fieldValue)}/`}>
-                    {tag.fieldValue} ({tag.totalCount})
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </div>
+          />
         </div>
       </div>
     </section>
   </Layout>
-)
+);
 
-export default TagsPage
+export default TagsPage;
 
 export const tagPageQuery = graphql`
   query TagsQuery {
@@ -42,11 +29,5 @@ export const tagPageQuery = graphql`
         title
       }
     }
-    allMarkdownRemark(limit: 1000) {
-      group(field: frontmatter___tags) {
-        fieldValue
-        totalCount
-      }
-    }
   }
-`
+`;
